a7acb76fa303232afa7515dd7c9cb0720b902151
Alexander <hello@alexander.works>
2021-03-27 01:12:50 +0200

    Implement adding to cart functionality

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,66 @@
+import { useState } from 'react';
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
 import products from '../products.json';
 import { initiateCheckout } from '../lib/payments.js';
 
+const defaultCart = {
+  products: {},
+};
+
 export default function Home() {
+  const [cart, updateCart] = useState(defaultCart);
+
+  const cartItems = Object.keys(cart.products).map((key) => {
+    const product = products.find(({ id }) => `${id}` === `${key}`);
+    return {
+      ...cart.products[key],
+      pricePerItem: product.price,
+    };
+  });
+
+  const roundTo = function (num, places) {
+    const factor = 10 ** places;
+    return Math.round(num * factor) / factor;
+  };
+
+  const subTotal = cartItems.reduce(
+    (accumulator, { pricePerItem, quantity }) => {
+      const amount = accumulator + pricePerItem * quantity;
+      return roundTo(amount, 2);
+    },
+    0
+  );
+
+  console.log('subTotal', subTotal);
+
+  function addToCart({ id } = {}) {
+    updateCart((prev) => {
+      let cartState = { ...prev };
+      if (cartState.products[id]) {
+        cartState.products[id].quantity = cartState.products[id].quantity + 1;
+      } else {
+        cartState.products[id] = {
+          id,
+          quantity: 1,
+        };
+      }
+
+      return cartState;
+    });
+  }
+
+  function checkout() {
+    initiateCheckout({
+      lineItems: cartItems.map((item) => {
+        return {
+          price: item.id,
+          quantity: item.quantity,
+        };
+      }),
+    });
+  }
+
   return (
     <div className={styles.container}>
       <Head>
@@ -18,6 +75,16 @@ export default function Home() {
           The best cupcakes in the universe! Yay! 🙌
         </p>
 
+        <p className={styles.description}>
+          <strong>Items:</strong> 2
+          <br />
+          <strong>Total Costs:</strong> ${subTotal}
+          <br />
+          <button className={styles.button_primary} onClick={checkout}>
+            Check Out
+          </button>
+        </p>
+
         <ul className={styles.grid}>
           {products.map((product) => {
             const { id, title, description, price, image } = product;
@@ -29,17 +96,12 @@ export default function Home() {
                 <p>${price}</p>
                 <p>{description}</p>
                 <button
-                  className={styles.button_primary}
-                  onClick={() =>
-                    initiateCheckout({
-                      lineItems: [
-                        {
-                          price: id,
-                          quantity: 1,
-                        },
-                      ],
-                    })
-                  }
+                  className={styles.button_secondary}
+                  onClick={() => {
+                    addToCart({
+                      id,
+                    });
+                  }}
                 >
                   Add To Cart
                 </button>
